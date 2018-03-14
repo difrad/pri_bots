bbfc00d97555c0d7937ea0dd0db20211bcf92c87
Dominic Difranzo <dominic@difranzo.com>
2018-03-14 13:26:24 -0400

    share too soon

--- js/BotNoShare.js
+++ js/BotNoShare.js
@@ -198,7 +198,7 @@
               //this.sendOutMessage('Ok!', 2000);
               this.sendOutMessage("Thank you.", 2500);
               //this.sendOutWait(2000);
-              this.sendOutMessage("Please continue taking the survey.",2500,true);
+              this.sendOutMessage("Please continue taking the survey.",3800,true);
               this.index++;
               $.ajax( { url: "https://api.mlab.com/api/1/databases/bots/collections/cons?apiKey=a4thkqnabYToz4TqxrG7RI8WNtG3IDJX",
                 data: JSON.stringify( this.data ),
